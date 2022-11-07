3bae6a7ab0c94017aa84cfd280f8def0caacacc9
JK369x <104179568+JK369x@users.noreply.github.com>
2022-11-07 21:59:49 +0700

    finish react-hook-useState

---
+++ image-carousel/src/components/ImageData.jsx
@@ -0,0 +1,19 @@
+const ImageData = [
+  {
+    title: "child",
+    image:
+      "https://cdn.pixabay.com/photo/2022/08/15/14/27/baby-7388056_1280.jpg",
+  },
+  {
+    title: "child2",
+    image:
+      "https://cdn.pixabay.com/photo/2016/01/20/11/11/baby-1151351_1280.jpg",
+  },
+  {
+    title: "child3",
+    image:
+      "https://cdn.pixabay.com/photo/2015/05/26/12/30/baby-784608_1280.jpg",
+  },
+];
+
+export default ImageData
